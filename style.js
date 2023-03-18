04a119356d7ff01a4a875c435b771e3d75882e8f
juancabj9 <126076078+juancabj9@users.noreply.github.com>
2023-03-18 11:45:03 +0100

    initicial commit

---
+++ style.js
@@ -0,0 +1,30 @@
+document.getElementById("list1").style.display = 'none';
+
+document.getElementById("list2").style.display = 'none';
+
+document.getElementById("list3").style.display = 'none';
+
+
+function mostrar(){
+  document.getElementById("list1").style.display = 'block'
+}
+
+function ocultar(){
+  document.getElementById("list1").style.display = 'none'
+}
+
+function mostrar2(){
+  document.getElementById("list2").style.display = 'block'
+}
+
+function ocultar2(){
+  document.getElementById("list2").style.display = 'none'
+}
+
+function mostrar3(){
+  document.getElementById("list3").style.display = 'block'
+}
+
+function ocultar3(){
+  document.getElementById("list3").style.display = 'none'
+}
